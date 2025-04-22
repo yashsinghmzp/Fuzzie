708f755b4c53c90e636d41e71d185faa0c5c8cf8
yash kumar singh <yashsinghkhanpur7@gmail.com>
2025-04-22 10:37:36 +0530

    second commit

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -1,6 +1,8 @@
 import { db } from '@/lib/db'
 import { NextResponse } from 'next/server'
 
+export const runtime = 'nodejs';
+
 export async function POST(req: Request) {
   try {
     const body = await req.json()
